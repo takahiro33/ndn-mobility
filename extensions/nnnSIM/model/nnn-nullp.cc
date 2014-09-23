967b4de1bed99d3188163f1cccc6cf02b932b5a8
Jairo Eduardo Lopez <jairo@ldc.usb.ve>
2014-09-23 14:45:53 +0900

    NULL packet license
    Got too happy pushing the packet and forgot to change the license.

--- extensions/nnnSIM/model/nnn-nullp.cc
+++ extensions/nnnSIM/model/nnn-nullp.cc
@@ -1,21 +1,21 @@
-/* -*- Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil -*- */
+/* -*- Mode: C++; c-file-style: "gnu" -*- */
 /*
- * Copyright (c) 2011 University of California, Los Angeles
+ * Copyright 2014 Waseda University, Sato Laboratory
+ *   Author: Jairo Eduardo Lopez <jairo@ruri.waseda.jp>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
+ *  nnn-nullp.cc is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  nnn-nullp.cc is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  You should have received a copy of the GNU Affero Public License
+ *  along with nnn-nullp.cc.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Author: Alexander Afanasyev <alexander.afanasyev@ucla.edu>
  */
 
 #include <ns3-dev/ns3/log.h>
--- extensions/nnnSIM/model/nnn-nullp.h
+++ extensions/nnnSIM/model/nnn-nullp.h
@@ -3,18 +3,18 @@
  * Copyright 2014 Waseda University, Sato Laboratory
  *   Author: Jairo Eduardo Lopez <jairo@ruri.waseda.jp>
  *
- *  nnn-so.h is free software: you can redistribute it and/or modify
+ *  nnn-nullp.h is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  nnn-so.h is distributed in the hope that it will be useful,
+ *  nnn-nullp.h is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Affero Public License for more details.
  *
  *  You should have received a copy of the GNU Affero Public License
- *  along with nnn-so.h.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with nnn-nullp.h.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
