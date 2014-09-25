2b14124bc190644b61abe99848c1c90d1136d054
Jairo Eduardo Lopez <jairo@ldc.usb.ve>
2014-09-25 22:39:42 +0900

    NNN EN packet header definition
    Mostly name changes. Compiles. Is not integrated with serialization yet.

--- extensions/nnnSIM/model/nnn-en.h
+++ extensions/nnnSIM/model/nnn-en.h
@@ -2,7 +2,8 @@
 /*
  * Copyright 2014 Waseda University, Sato Laboratory
  *   Author: Jairo Eduardo Lopez <jairo@ruri.waseda.jp>
- *	     Zhu Li <philipszhuli1990@ruri.waseda.jp>
+ *	         Zhu Li <philipszhuli1990@ruri.waseda.jp>
+ *
  *  nnn-en.h is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
@@ -15,16 +16,17 @@
  *
  *  You should have received a copy of the GNU Affero Public License
  *  along with nnn-en.h.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 #ifndef _NNN_EN_HEADER_H_
 #define _NNN_EN_HEADER_H_
 
+#include <vector>
+
 #include <ns3-dev/ns3/simple-ref-count.h>
 #include <ns3-dev/ns3/nstime.h>
 #include <ns3-dev/ns3/packet.h>
-#include  <ns3-dev/ns3/ptr.h>
+#include <ns3-dev/ns3/ptr.h>
 #include <ns3-dev/ns3/mac48-address.h>
 
 #include "nnn-address.h"
@@ -39,13 +41,13 @@ namespace nnn {
  * @ingroup nnn
  * @brief NNN EN packet (wire formats are defined in wire)
  **/
-class EN : public SimpleRefCount<en>
+class EN : public SimpleRefCount<EN>
 {
 public:
 	/**
 	 * \brief Constructor
 	 *
-	 * Creates a EN packet with no payload
+	 * Creates a EN packet
 	 **/
 	EN ();
 
@@ -53,9 +55,9 @@ public:
 	 * \brief Constructor
 	 *
 	 *
-	 * @param name MAC vector
+	 * @param name MAC Address signature creator
 	 **/
-	EN (std::vector<MACAddress48> signature);
+	EN (std::vector<Mac48Address> signature);
 
 	/**
 	 * @brief Copy constructor
@@ -66,8 +68,11 @@ public:
 	 * \brief Return Id of the packet
 	 *
 	 **/
-	unit32_t
+	uint32_t
 	GetPacketId ();
+
+	uint16_t
+	GetPoaType () const;
 
 	/**
 	 * \brief Add Signature(MAC)
@@ -76,10 +81,10 @@ public:
 	 *
 	 **/
 	void
-	AddSig (std::vector<MACAddress48> signature);
+	AddPoa (Mac48Address signature);
 
 	void
-	AddSig (MACAddress48 signature);
+	AddPoa (std::vector<Mac48Address> signatures);
 
 	/**
 	 * \brief Get number of MN's Signatures
@@ -87,18 +92,18 @@ public:
 	 * @param  const reference to Name object
 	 *
 	 **/
-    unit32_t
-    GetNumSig () const;
+    uint32_t
+    GetNumPoa () const;
 
 	/**
 	 * \brief Get Signatures of MN
 	 *
 	 **/
-    std::vector<MACAddress48>
-	GetSigs () const;
+    std::vector<Mac48Address>
+	GetPoas () const;
 
-    MACAddress48
-    GetOneSig (int index) const;
+    Mac48Address
+    GetOnePoa (uint32_t index) const;
 
 	/**
 	 * \brief Set time out for EN packet
@@ -149,9 +154,8 @@ private:
 private:
 	uint32_t m_packetid;      ///< @brief Packet Identifier (3 for EN)
 	Time m_ttl;               ///< @brief Packet life time (TTL)
-	uint32_t m_numsign;   ///< @brief number of MN's Signatures (MAC)
-	std::vector<MACAddress> m_mac_signs;  ///<@brief vactor of Signatures
-	Ptr<NNNAddress> m_name;   ///< @brief Destination NNN Address used in the packet
+	uint16_t m_poa_type;      ///< @brief Type of PoA in EN packet
+	std::vector<Mac48Address> m_poas;  ///<@brief vector of Signatures
 
 	mutable Ptr<const Packet> m_wire;
 };
