b9a24a9e8fae96bc94c26e6e4174048cfe00da6a
Jairo Eduardo Lopez <jairo@ldc.usb.ve>
2014-09-27 17:42:08 +0900

    Added nnn-nnnsim-wire.h to summarize headers in Wire

---
+++ extensions/nnnSIM/model/nnn-nnnsim-wire.h
@@ -0,0 +1,27 @@
+/* -*- Mode:C++; c-file-style:"gnu" -*- */
+/*
+ * Copyright 2014 Waseda University, Sato Laboratory
+ *   Author: Jairo Eduardo Lopez <jairo@ruri.waseda.jp>
+ *
+ *  nnn-nnnsim-wire.h is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  nnn-nnnsim-wire.h is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero Public License
+ *  along with nnn-nnnsim-wire.h.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NNN_NNNSIM_WIRE_H_
+#define NNN_NNNSIM_WIRE_H_
+
+#include "wire/nnn-wire.h"
+#include "wire/wire-nnnsim.h"
+#include "wire/nnnsim/nnnsim.h"
+
+#endif
