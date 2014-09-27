ffac3dcb42b589b81eacd0da9526f3fcd895b129
Jairo Eduardo Lopez <jairo@ldc.usb.ve>
2014-09-27 17:44:00 +0900

    Added nnn-naming to summarize naming directory headers

---
+++ extensions/nnnSIM/model/nnn-naming.h
@@ -0,0 +1,31 @@
+/* -*- Mode:C++; c-file-style:"gnu" -*- */
+/*
+ * Copyright 2014 Waseda University, Sato Laboratory
+ *   Author: Jairo Eduardo Lopez <jairo@ruri.waseda.jp>
+ *
+ *  nnn-naming.h is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  nnn-naming.h is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero Public License
+ *  along with nnn-naming.h.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef NNN_NAMING_H_
+#define NNN_NAMING_H_
+
+#include "naming/blob.h"
+#include "naming/error.h"
+#include "naming/name-component.h"
+#include "naming/nnn-address.h"
+#include "naming/nnn-name-format.h"
+
+
+#endif /* NNN_NAMING_H_ */
