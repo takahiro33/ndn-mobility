a66d72e85186db74db8d53f96cc11c46b9687aaf
Jairo Eduardo Lopez <jelfn@sislacom.com>
2014-11-21 19:03:14 +0900

    Creation of NamesContainer test file
    I place to implement a test file in nnn-names-container-test for NamesContainer class.

---
+++ scenarios/nnn-names-container-test.cc
@@ -0,0 +1,27 @@
+/* -*- Mode:C++; c-file-style:"gnu" -*- */
+/*
+ * Copyright 2014 Waseda University, Sato Laboratory
+ *   Author: Jairo Eduardo Lopez <jairo@ruri.waseda.jp>
+ *
+ *  nnn-names-cointainer-test.cc is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  nnn-names-cointainer-test.cc is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero Public License
+ *  along with nnn-names-cointainer-test.cc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Extensions
+#include "nnnSIM/nnnSIM-module.h"
+
+int main (int argc, char *argv[])
+{
+
+}
